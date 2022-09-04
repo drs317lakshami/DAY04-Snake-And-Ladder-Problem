3b061669863284feabb255b46ddbbc37917f49f3
drs317lakshami <devrajkumar317@gmail.com>
2022-09-04 12:49:56 +0530

    Welcome To The Snake And Ladder Program

---
+++ SnakeAndLadders.java
@@ -0,0 +1,10 @@
+package bridgelabz.day4.com;
+
+public class SnakeAndLadders {
+
+    public static void main(String [] args){
+
+        System.out.println("Welcome To Snake And Ladder Simulator");
+    }
+
+}
