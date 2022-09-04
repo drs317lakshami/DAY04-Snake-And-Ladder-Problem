752c5ad6cf614d540ddefdd7cc39c6d1b45260fb
drs317lakshami <devrajkumar317@gmail.com>
2022-09-04 12:53:49 +0530

    Adding:Start The Position 0

--- SnakeAndLadders.java
+++ SnakeAndLadders.java
@@ -4,7 +4,8 @@ public class SnakeAndLadders {
 
     public static void main(String [] args){
 
-        System.out.println("Welcome To Snake And Ladder Simulator");
+        int startPosition = 0;
+        System.out.println("Starting Position is :" + startPosition);
     }
 
 }
